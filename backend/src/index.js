dc80f434417393ff786f9d21fdcdc940fe337cf0
Rohit Kiran P. <rohitkiran2023@gmail.com>
2026-03-16 23:18:43 +0530

    completed Backend setup and Cloudinary config

---
+++ backend/src/index.js
@@ -0,0 +1,24 @@
+// const express=require('express'); instead of this we can use import express from 'express'; if we add "type":"module" in package.json file
+import express from 'express';
+import authRoutes from './routes/auth.routes.js';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+
+import {connectDB} from './lib/db.js';
+
+
+dotenv.config();
+
+const app=express();
+const PORT=process.env.PORT
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth",authRoutes)
+
+app.listen(PORT,()=>{
+    console.log("server is runing on port "+PORT);
+    connectDB();
+});
