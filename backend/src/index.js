b136d3777cbfe01d323869f413f10fcc8155bd91
Rohit Kiran P. <rohitpolicetti@gmail.com>
2026-03-17 21:53:29 +0530

    Added message controller

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 // const express=require('express'); instead of this we can use import express from 'express'; if we add "type":"module" in package.json file
 import express from 'express';
 import authRoutes from './routes/auth.routes.js';
+import messageRoutes from './routes/message.routes.js';
+
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 
@@ -17,6 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth",authRoutes)
+app.use("/api/messages",messageRoutes)
 
 app.listen(PORT,()=>{
     console.log("server is runing on port "+PORT);
